aa32268c5ebc771a733f82f50523583fcc4044c1
Ricardo <ing.ricardo.vi@gmail.com>
2020-09-07 14:00:42 -0500

    Changes pins for PDM

--- variants/BASTBLE/variant.cpp
+++ variants/BASTBLE/variant.cpp
@@ -125,7 +125,29 @@ extern "C" {
 #include "nrf_uarte.h"
 #include "nrf_uart.h"
 
-void initVariant() { 
+void initVariant() {
+  // turn power LED on
+  //pinMode(LED_PWR, OUTPUT);
+  //digitalWrite(LED_PWR, HIGH);
+
+  // Errata Nano33BLE - I2C pullup is on SWO line, need to disable TRACE
+  // was being enabled by nrfx_clock_anomaly_132
+  //CoreDebug->DEMCR = 0;
+  //NRF_CLOCK->TRACECONFIG = 0;
+
+  // FIXME: bootloader enables interrupt on COMPARE[0], which we don't handle
+  // Disable it here to avoid getting stuck when OVERFLOW irq is triggered
+  //nrf_rtc_event_disable(NRF_RTC1, NRF_RTC_INT_COMPARE0_MASK);
+  //nrf_rtc_int_disable(NRF_RTC1, NRF_RTC_INT_COMPARE0_MASK);
+
+  // FIXME: always enable I2C pullup and power @startup
+  // Change for maximum powersave
+  //pinMode(PIN_ENABLE_SENSORS_3V3, OUTPUT);
+  //pinMode(PIN_ENABLE_I2C_PULLUP, OUTPUT);
+
+  //digitalWrite(PIN_ENABLE_SENSORS_3V3, HIGH);
+  //digitalWrite(PIN_ENABLE_I2C_PULLUP, HIGH);
+ 
   NRF_PWM_Type* PWM[] = {
     NRF_PWM0, NRF_PWM1, NRF_PWM2
 #ifdef NRF_PWM3
